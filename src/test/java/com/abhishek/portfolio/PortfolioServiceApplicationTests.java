5bc6b2a8fc874f45a1c924a6bedbe413fc522a2a
Abhishek Priyadarshi <abhishekpriyadarshi@Abhisheks-MacBook-Pro.local>
2026-04-12 17:53:52 +0530

    Initial portfolio service setup

---
+++ src/test/java/com/abhishek/portfolio/PortfolioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
